f4e59c99760697df5b42e810e1dbd7eef92675a5
Claudio Altamura <claudio@claudioaltamura.de>
2020-12-10 13:23:23 +0100

    chg: changed jdk for travis

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ dependencies {
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_15
-  targetCompatibility = JavaVersion.VERSION_15
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
 }
 
 
