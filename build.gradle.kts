fad2572e03bfcaddcff1b898097fb96f95de08ba
Claudio Altamura <claudio@claudioaltamura.de>
2021-01-09 11:21:12 +0100

    chore: upgrade libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,14 @@ repositories {
 }
 
 dependencies {
+	//implementation ("io.reactivex.rxjava2:rxjava:2.2.20")
+	implementation ("io.reactivex:rxjava:1.3.8")
+
 	testImplementation("org.assertj:assertj-core:3.18.1")
 
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
 
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
 
 java {
