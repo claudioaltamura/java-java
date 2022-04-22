f3f72467933dbd9d01925445eeb8ca13f1b4875e
Claudio Altamura <claudio@claudioaltamura.de>
2022-04-22 20:02:58 +0200

    chore: house keeping

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
   java
   idea
   eclipse
-	id ("com.github.ben-manes.versions") version "0.39.0"
+    id("com.diffplug.spotless") version "6.4.2"
+    id("com.github.ben-manes.versions") version "0.42.0"
+
 }
 
 repositories {
@@ -13,20 +15,14 @@ repositories {
 
 dependencies {
 
-	testImplementation("org.assertj:assertj-core:3.20.2")
+	testImplementation("org.assertj:assertj-core:3.22.0")
 
     testImplementation("org.hamcrest:hamcrest:2.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
-
-java {
-  sourceCompatibility = JavaVersion.VERSION_11
-  targetCompatibility = JavaVersion.VERSION_11
-}
-
 
 val test by tasks.getting(Test::class) {
   useJUnitPlatform()
