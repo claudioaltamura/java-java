90328a129efaeac5c071c3794ef150989af23ca8
Claudio Altamura <claudio@claudioaltamura.de>
2022-09-09 08:48:33 +0200

    new: disjoint example

--- src/test/java/de/claudioaltamura/java/solutions/DifferencesInListTest.java
+++ src/test/java/de/claudioaltamura/java/solutions/DifferencesInListTest.java
@@ -2,6 +2,8 @@ package de.claudioaltamura.java.solutions;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +21,23 @@ class DifferencesInListTest {
     final List<String> differences = DifferencesInList.differencesWithStream(List.of("Peter", "Bruce", "Clark"), List.of("Peter", "Clark"));
 
     assertThat(differences).contains("Bruce");
+  }
+
+  @Test
+  void testDisjoint() {
+    final var list1 = new ArrayList<>();
+    list1.add("A");
+    list1.add("B");
+
+    final var list2 = new ArrayList<>();
+    list2.add("A");
+    list2.add("B");
+    list2.add("c");
+
+    assertThat(Collections.disjoint(list1, list2)).isFalse();
+
+
+
   }
 
 }
