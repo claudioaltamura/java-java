13ec3f5d90fc7a6c039e3d362059acf115a710e3
Claudio Altamura <claudio@claudioaltamura.de>
2022-09-09 08:48:46 +0200

    chore: upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,15 +13,20 @@ repositories {
   mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 dependencies {
 
 	testImplementation("org.assertj:assertj-core:3.22.0")
 
     testImplementation("org.hamcrest:hamcrest:2.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 val test by tasks.getting(Test::class) {
