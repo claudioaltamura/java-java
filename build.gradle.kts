2eceaf19d270a395405ba8b0a955b31d6102533d
claudioaltamura <claudio@claudioaltamura.de>
2024-08-09 09:32:07 +0200

    build: upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
   java
   idea
   eclipse
-    id("com.diffplug.spotless") version "6.18.0"
-    id("com.github.ben-manes.versions") version "0.46.0"
+    id("com.diffplug.spotless") version "6.25.0"
+    id("com.github.ben-manes.versions") version "0.51.0"
 
 }
 
@@ -14,19 +14,19 @@ repositories {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 dependencies {
 
-	testImplementation("org.assertj:assertj-core:3.24.2")
+	testImplementation("org.assertj:assertj-core:3.26.3")
 
     testImplementation("org.hamcrest:hamcrest:2.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.3")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.3")
 }
 
 val test by tasks.getting(Test::class) {
