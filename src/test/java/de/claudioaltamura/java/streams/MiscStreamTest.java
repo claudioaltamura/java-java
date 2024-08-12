0db75a1a87a283be16431144cf754587f599c9ba
claudioaltamura <claudio@claudioaltamura.de>
2024-08-12 16:23:17 +0200

    feat: misc examples

---
+++ src/test/java/de/claudioaltamura/java/streams/MiscStreamTest.java
@@ -0,0 +1,50 @@
+package de.claudioaltamura.java.streams;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MiscStreamTest {
+
+	@Test
+	void shouldReturnFirstElement() {
+		assertEquals(Stream.of("a1", "a2", "a3")
+				.findFirst().orElseThrow(), "a1");
+	}
+
+	@Test
+	void shouldCalculateAverage() {
+		assertEquals(Arrays.stream(new int[] {1, 2, 3})
+				.map(n -> 2 * n + 1)
+				.average().orElseThrow(),5.0D);
+	}
+
+	@Test
+	void shouldFilter() {
+		Stream.of("d2", "a2", "b1", "b3", "c")
+				.filter(s -> {
+					System.out.println("filter: " + s);
+					return true;
+				})
+				.forEach(s -> System.out.println("forEach: " + s));
+	}
+
+	@Test
+	void shouldMapAndAnyMatch() {
+		assertTrue(Stream.of("d2", "a2", "b1", "b3", "c")
+				.map(String::toUpperCase)
+				.anyMatch(s -> s.startsWith("A")));
+	}
+
+	@Test
+	void shouldSortAndFilter() {
+		assertEquals(Stream.of("d2", "a2", "b1", "b3", "c")
+				.filter(s -> s.startsWith("a"))
+				.sorted(String::compareTo)
+				.map(String::toUpperCase)
+				.toList(), List.of("A2"));
+	}
+}
