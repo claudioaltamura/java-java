f266752ed149cc137ebf2737970cb8b175f5c91b
claudioaltamura <claudio@claudioaltamura.de>
2024-12-04 14:36:26 +0100

    chore: upgrade libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,11 +22,11 @@ dependencies {
 
 	testImplementation("org.assertj:assertj-core:3.26.3")
 
-    testImplementation("org.hamcrest:hamcrest:2.2")
+    testImplementation("org.hamcrest:hamcrest:3.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
 }
 
 val test by tasks.getting(Test::class) {
